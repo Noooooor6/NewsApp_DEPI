81a4e37cf513ad9e091b7d455dc3b4766e12619c
HudaHussamEldeen <huda.hussameldeen@gmail.com>
2025-10-15 02:03:34 +0100

    splash gif added and lunch icon

--- app/src/main/java/com/example/newsapp/SplashActivity.kt
+++ app/src/main/java/com/example/newsapp/SplashActivity.kt
@@ -1,20 +1,33 @@
 package com.example.newsapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.example.newsapp.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+    private val SPLASH_DURATION: Long = 10000 // 10 seconds
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Load GIF animation into ImageView
+        Glide.with(this)
+            .asGif()
+            .load(R.drawable.splash) // your GIF file (splash.gif) in drawable folder
+            .into(binding.splashGif)
+
+        // After 10 seconds, move to SignInActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+        }, SPLASH_DURATION)
     }
 }
